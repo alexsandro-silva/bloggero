9ae85056beeb57d0d03d6e37e4632b22c9a8f295
Alexsandro Rodrigues <alexsandrorodriguess@gmail.com>
2022-02-01 02:20:35 -0300

    helper para exibir data por extenso

--- application/helpers/funcoes_helper.php
+++ application/helpers/funcoes_helper.php
@@ -1,6 +1,8 @@
 ﻿<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-function limpar($string){
+
+function limpar($string)
+{
 	$table = array(
         '/'=>'', '('=>'', ')'=>'',
     );
@@ -11,4 +13,31 @@ function limpar($string){
 	$string= str_replace(" ", "-", $string);
 	$string= str_replace("---", "-", $string);
 	return $string;
+}
+
+function postadoem($string)
+{
+    $dias_semana = array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
+    $meses = array(
+        '01' => "Janeiro",
+        '02' => "Fevereiro",
+        '03' => "Março",
+        '04' => "Abril",
+        '05' => "Maio",
+        '06' => "Junho",
+        '07' => "Julho",
+        '08' => "Agosto",
+        '09' => "Setembro",
+        '10' => "Outubro",
+        '11' => "Novembro",
+        '12' => "Dezembro"
+    );
+
+    $dia_sem = date('w', strtotime($string));
+    $dia = date('d', strtotime($string));
+    $mes_num = date('m', strtotime($string));
+    $ano = date('Y', strtotime($string));
+    $hora = date('H:i', strtotime($string));
+
+    return $dias_semana[$dia_sem] . ', ' . $dia . ' de ' . $meses[$mes_num] . ' de ' . $ano . ' ' . $hora;
 }
